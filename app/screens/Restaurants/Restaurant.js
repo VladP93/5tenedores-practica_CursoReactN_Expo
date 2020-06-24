df55f5a2543c06e81d30cfadafb3d3ca00825fcc
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-06-24 03:19:41 -0600

    Seccion 11 finalizada/ Geolocalización e info extra

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import { map } from "lodash";
+import { Rating, ListItem, Icon } from "react-native-elements";
 import Loading from "../../components/Loading";
 import Carousel from "../../components/Carousel";
+import Map from "../../components/Map";
 
 import { firebaseApp } from "../../utils/firebase";
 import firebase from "firebase/app";
@@ -15,6 +18,7 @@ export default function Restaurant(props) {
   const { id, name } = route.params;
 
   const [restaurant, setRestaurant] = useState(null);
+  const [rating, setRating] = useState(0);
 
   useEffect(() => {
     db.collection("restaurants")
@@ -24,6 +28,7 @@ export default function Restaurant(props) {
         const data = response.data();
         data.id = response.id;
         setRestaurant(data);
+        setRating(data.rating);
       });
   }, []);
 
@@ -38,10 +43,101 @@ export default function Restaurant(props) {
         height={250}
         width={screenWidth}
       />
+      <TitleRestaurant
+        name={restaurant.name}
+        description={restaurant.description}
+        rating={rating}
+      />
+      <RestaurantInfo
+        location={restaurant.location}
+        name={restaurant.name}
+        address={restaurant.address}
+      />
     </ScrollView>
+  );
+}
+
+function TitleRestaurant(props) {
+  const { name, description, rating } = props;
+  return (
+    <View style={styles.viewRestaurantTitle}>
+      <View style={{ flexDirection: "row" }}>
+        <Text style={styles.nameRestaurant}>{name}</Text>
+        <Rating
+          style={styles.rating}
+          imageSize={20}
+          readonly={true}
+          startingValue={parseFloat(rating)}
+        />
+      </View>
+      <Text style={styles.descriptionRestaurant}>{description}</Text>
+    </View>
+  );
+}
+
+function RestaurantInfo(props) {
+  const { location, name, address } = props;
+
+  const listInfo = [
+    {
+      text: address,
+      iconName: "map-marker",
+      iconType: "material-community",
+      action: null,
+    },
+  ];
+
+  return (
+    <View style={styles.viewRestaurantInfo}>
+      <Text style={styles.restaurantInfoTitle}>
+        Información sobre el restaurante
+      </Text>
+      <Map location={location} name={name} height={100} />
+
+      {map(listInfo, (item, index) => (
+        <ListItem
+          key={index}
+          title={item.text}
+          leftIcon={{
+            name: item.iconName,
+            type: item.iconType,
+            color: "#00a680",
+          }}
+          containerStyle={styles.containerListItem}
+        />
+      ))}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   viewBody: { flex: 1, backgroundColor: "#ffffff" },
+  viewRestaurantTitle: {
+    padding: 15,
+  },
+  nameRestaurant: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  descriptionRestaurant: {
+    marginTop: 5,
+    color: "grey",
+  },
+  rating: {
+    position: "absolute",
+    right: 0,
+  },
+  viewRestaurantInfo: {
+    margin: 15,
+    marginTop: 25,
+  },
+  restaurantInfoTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  containerListItem: {
+    borderBottomColor: "#d8d8d8",
+    borderBottomWidth: 1,
+  },
 });
